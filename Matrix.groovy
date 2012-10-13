5900475117ab804a5fa7efd5db298c686d8fc68c
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-13 19:18:22 +0100

    Nearly done with ex3. Just need to do toString method.

--- Matrix.groovy
+++ Matrix.groovy
@@ -1,18 +1,18 @@
 class Matrix {
     int[][] array2d
+    int[][] lengths2d
     int width, height
     
     Matrix(int new_width, int new_height) {
         width = new_width
         height = new_height
         verifySize()
-            
-        for (x in 0..width-1) {
-            array2d.add([1]*height as int[])
-        }
+        
+        array2d = [[1]*height]*width
+        lengths2d = [[1]*height]*width
     }
     
-    void verifySize() {
+    private void verifySize() {
         if (width < 1)
             throw new BadInput("Width of matrix must be greater than 1")
         if (height < 1)
@@ -22,32 +22,56 @@ class Matrix {
     void setElement(int x, int y, int value) {
         try {
             array2d[x][y] = value
+            lengths2d[x][y] = (value as String).length()
         } catch(IndexOutOfBoundsException) {}
     }
     
     void setRow(int y, String row_str) {
-        int[] row_array = row_str.split(",")*.toInteger()
+        int[] row_array = strToArray(row_str)
         
         if (row_array.size() != width)
             return
             
-        try {
-            for (x in 0..width-1) {
-                array2d[x][y] = value
-            }
-        } catch(IndexOutOfBoundsException) {}
+        for (x in 0..width-1) {
+            setElement(x, y, row_array[x])
+        }
     }
     
-    void prettyPrint() {
-        println "-"*width*8
+    private int[] strToArray(String str) {
+        return str.split(",")*.toInteger()
+    }
+    
+    void setColumn(int x, String column_str) {
+        int[] column_array = strToArray(column_str)
+        
+        if (column_array.size() != height)
+            return
+            
         for (y in 0..height-1) {
-            print "-"*width*8 + "\n|"
-            for (x in 0..width-1) {
-                print "\t" + array2d[x][y] + "|"
-            }
+            setElement(x, y, column_array[y])
         }
+    }
+    
+    void toString() {
+        
+    
+    void prettyPrint() {
+        int cell_width = lengths2d.flatten().max()
+        int left_spaces, right_spaces
+        String row_line = ("+" + "-"*(cell_width))*width + "+"
+        
+        for (y in 0..height-1) {
+            //print "-"*(1+width*(cell_width+1)) + "\n|" // The plus-one is for the "|"
+            print row_line + "\n|"
+            for (x in 0..width-1) {
+                 right_spaces = ( cell_width - lengths2d[x][y] ) / 2
+                left_spaces = cell_width - lengths2d[x][y] - right_spaces
+                print " "*left_spaces + array2d[x][y] + " "*right_spaces + "|"
+            }
+            print "\n"
+        }
+        println row_line
     }
                 
 }
-
 
--- ex3_creating_matrices.groovy
+++ ex3_creating_matrices.groovy
@@ -1,3 +1,4 @@
 Matrix m = new Matrix(3,4)
-
+m.setRow(1, "2,33,444")
+m.setColumn(0, "555,6666,77777,888888")
 m.prettyPrint()
